761f53f053492b249f84c90ca39edf0a609caaf4
Jarrod <jsgriffin96@live.com>
2020-10-20 13:39:25 -0700

    Added importing data from URL

--- homework_script.R
+++ homework_script.R
@@ -1,6 +1,6 @@
 library('tidyverse')
-#import data
-cars <- read_csv("data/fleet-asset-management-system-open-data-2015-2019.csv")
+#import data from website
+cars <- read_csv("https://data.ca.gov/dataset/1b31c08e-b1a7-4459-8aef-41cfff61fc5e/resource/5c42e9f6-e172-4db4-9a51-ca1256b03a26/download/fleet-asset-management-system-open-data-2015-2019.csv")
 
 #convert purchase price from char to number
 cars['Purchase_Price'] <- parse_number(cars[['Purchase_Price']])
